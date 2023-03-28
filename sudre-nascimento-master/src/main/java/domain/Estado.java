dffe60bf9236b4ff0f65068dd963058e33970282
Jonatas Lima Barbosa <67673863+Jonatas1678@users.noreply.github.com>
2023-03-28 20:31:37 -0300

    Add files via upload

---
+++ sudre-nascimento-master/src/main/java/domain/Estado.java
@@ -0,0 +1,75 @@
+package domain;
+
+
+import jakarta.persistence.*;
+
+import javax.lang.model.element.Name;
+
+@Entity
+@Table(name = "TB_ESTADO",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_NM_ESTADO", columnNames = "NM_ESTADO"),
+                @UniqueConstraint(name = "UK_SG_ESTADO", columnNames = "SG_ESTADO")
+        }
+)
+public class Estado {
+
+
+    @Id
+    @SequenceGenerator(name = "SQ_ESTADO", sequenceName = "SQ_ESTADO")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ESTADO")
+    @Column(name = "ID_ESTADO")
+    private int id;
+
+    @Column(name = "NM_ESTADO")
+    private String nome;
+
+    @Column(name = "SG_ESTADO")
+    private String sigla;
+
+    public Estado() {
+    }
+
+    public Estado(int id, String nome, String sigla) {
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Estado setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Estado setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public Estado setSigla(String sigla) {
+        this.sigla = sigla;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Estado{");
+        sb.append("id=").append(id);
+        sb.append(", nome='").append(nome).append('\'');
+        sb.append(", sigla='").append(sigla).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ sudre-nascimento-master/src/main/java/domain/Processo.java
@@ -0,0 +1,122 @@
+package domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "TB_PROCESSO",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_NR_PROCESSO", columnNames = "NR_PROCESSO")
+        }
+)
+public class Processo {
+
+    @Id
+    @SequenceGenerator(name = "SQ_PROCESSO", sequenceName = "SQ_PROCESSO")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PROCESSO")
+    @Column(name = "ID_PROCESSO")
+    private long id;
+
+    @Column(name = "NR_PROCESSO")
+    private String numero;
+
+    @Column(name = "DT_DISTRIBUICAO")
+    private LocalDate dataDeDistribuicao;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "ID_TP_ACAO", referencedColumnName = "ID_TP_ACAO",
+            foreignKey = @ForeignKey(name = "FK_PROC_TP_ACAO", value = ConstraintMode.CONSTRAINT)
+    )
+    private TipoDeAcao tipoDeAcao;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "ID_ADVOGADO", referencedColumnName = "ID_ADVOGADO",
+            foreignKey = @ForeignKey(name = "FK_PROC_ADVOGADO", value = ConstraintMode.CONSTRAINT)
+    )
+    private Advogado advogado;
+
+    @Column(name = "PRO_BONO")
+    private boolean proBono;
+
+
+    public Processo() {
+    }
+
+    public Processo(long id, String numero, LocalDate dataDeDistribuicao, TipoDeAcao tipoDeAcao, Advogado advogado, boolean proBono) {
+        this.id = id;
+        this.numero = numero;
+        this.dataDeDistribuicao = dataDeDistribuicao;
+        this.tipoDeAcao = tipoDeAcao;
+        this.advogado = advogado;
+        this.proBono = proBono;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Processo setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public Processo setNumero(String numero) {
+        this.numero = numero;
+        return this;
+    }
+
+    public LocalDate getDataDeDistribuicao() {
+        return dataDeDistribuicao;
+    }
+
+    public Processo setDataDeDistribuicao(LocalDate dataDeDistribuicao) {
+        this.dataDeDistribuicao = dataDeDistribuicao;
+        return this;
+    }
+
+    public TipoDeAcao getTipoDeAcao() {
+        return tipoDeAcao;
+    }
+
+    public Processo setTipoDeAcao(TipoDeAcao tipoDeAcao) {
+        this.tipoDeAcao = tipoDeAcao;
+        return this;
+    }
+
+    public Advogado getAdvogado() {
+        return advogado;
+    }
+
+    public Processo setAdvogado(Advogado advogado) {
+        this.advogado = advogado;
+        return this;
+    }
+
+    public boolean isProBono() {
+        return proBono;
+    }
+
+    public Processo setProBono(boolean proBono) {
+        this.proBono = proBono;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Processo{");
+        sb.append("id=").append(id);
+        sb.append(", numero='").append(numero).append('\'');
+        sb.append(", dataDeDistribuicao=").append(dataDeDistribuicao);
+        sb.append(", tipoDeAcao=").append(tipoDeAcao);
+        sb.append(", advogado=").append(advogado);
+        sb.append(", proBono=").append(proBono);
+        sb.append('}');
+        return sb.toString();
+    }
+}
